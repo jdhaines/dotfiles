422997c5a1eb77c6aafdd4dcea35b2ceadeea7be
Josh Haines <Josh@JoshHaines.com>
2022-10-17 09:58:38 -0400

    refactor(packer bootstrap): based on other dotfiles

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -51,20 +51,46 @@ keymap('v', 'S', '<cmd>HopChar2BC<CR>', opts)
 ----
 -- Install & Configure Packer Plugin Manager
 ----
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+
+local settings = require("user-conf")
+local fn = vim.fn
+
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+-- returns the require for use in `config` parameter of packer's use
+-- expects the name of the config file
+local function get_config(name)
+  return string.format('require("config/%s")', name)
 end
 
-local packer_bootstrap = ensure_packer()
+-- bootstrap packer if not installed
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({
+    "git",
+    "clone",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer...")
+  vim.api.nvim_command("packadd packer.nvim")
+end
 
-return require('packer').startup(function(use)
+-- initialize and configure packer
+local packer = require("packer")
+
+packer.init({
+  enable = true, -- enable profiling via :PackerCompile profile=true
+  threshold = 0, -- the amount in ms that a plugins load time must be over for it to be included in the profile
+  max_jobs = 20, -- Limit the number of simultaneous jobs. nil means no limit. Set to 20 in order to prevent PackerSync form being "stuck" -> https://github.com/wbthomason/packer.nvim/issues/746
+  -- Have packer use a popup window
+  display = {
+    open_fn = function()
+      return require("packer.util").float({ border = "rounded" })
+    end,
+  },
+})
+
+packer.startup(function(use)
 
   use 'wbthomason/packer.nvim'
 
